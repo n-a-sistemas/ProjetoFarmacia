8ceb6cdfd14f55646c3d6afc1e113fe4460fb153
LEONARDO HENRIQUE TOCHIO <leonardo.htochio@senacsp.edu.br>
2020-01-30 17:21:55 -0300

    Início CSS

--- HTML/index.php
+++ HTML/index.php
@@ -5,34 +5,82 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
-    <script src="JS/jquery-3.4.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="JS/login.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
 </head>
 <body>
-    
     <header>
-        <h1>Login</h1>
+        <div class="collapse" id="navbarHeader" style>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-8 col-md-7 py-4">
+                        <h4 class="text-secondary">Farmácia</h4>
+                        <p class="text-muted">#</p>
+                    </div>
+                    <div class="col-sm-4 offset-md-1 py-4">
+                        <h4 class="text-secondary">Menu</h4>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="index.php" class="text-secondary">Pagina inicial</a>
+                            </li>
+                            <li>
+                                <a href="contato.php" class="text-secondary">Contato</a>
+                            </li>
+                            <li>
+                                <a href="cadastro.php" class="text-secondary">Cadastro</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="navbar navbar-dark shadows-sm">
+            <div class="container d-flex justify-content-between">
+                <a href="index.php" class="navbar-brand d-flex align-items-center">
+                    <img src="IMG/logo.svg" alt="logo" width="50" height="50" class="d-inline-block align-top">
+                    <strong class="text-danger">Fármacia</strong>
+                </a>
+                <button class="navbar-toggler collapsed bg-danger" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </div>
+        <div class="navbar navbar-dark bg-danger shadows-sm">
+            <div class="container d-flex justify-content-between"></div>
+        </div>
     </header>
+    <div class="container bg-danger d-flex justify-content-center w-25 mt-5">
+        <div class="row">
+            <main>
+                <form action="../PHP/logar.php" method="post" onsubmit="return validaLogin();">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text" id="btnGroupAddon">
 
-    <?php include("./parts/navegacao.php"); ?>
+                            </div>
+                        </div>
+                        <input type="email" class="form-control" placeholder="Email" name="email" id="email" required>
+                    </div>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text" id="btnGroupAddon">
 
-    <main>
-        <form action="../PHP/logar.php" method="post" onsubmit="return validaLogin();">
-            <div>
-                <label for="email">Email: </label>
-                <input type="email" name="email" id="email" required>
-            </div>
-            <div>
-                <label for="senha">Senha: </label>
-                <input type="password" name="senha" id="senha" required>
-                <button type="button" id="btn">Mostrar senha</button>
-            </div>
-            <div>
-                <a href="cadastro.php">Não é cadastrado ?</a>
-                <a href="#">Não sabe a senha ?</a>
-                <button type="submit" id="logar">Logar</button>
-            </div>
-        </form>
-    </main>
+                            </div>
+                        </div>
+                        <input type="password" class="form-control" placeholder="Senha" name="senha" id="senha" required>
+                        <button type="button" id="btn">Mostrar senha</button>
+                    </div>
+                    <div>
+                        <a href="cadastro.php" class="text-light">Não é cadastrado?</a>
+                        <a href="#" class="text-light">Não sabe a senha?</a>
+                        <button type="submit" id="logar">Logar</button>
+                    </div>
+                </form>
+            </main>
+        </div>
+    </div>
 </body>
 </html>
