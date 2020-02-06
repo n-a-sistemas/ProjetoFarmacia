e67752003a7014f0e0bb23bf42dd3e7fc90c0b45
Kevyn02 <kaparecido483@gmail.com>
2020-02-05 23:53:35 -0200

    05/02 - Validando a parte de enviar email

--- PHP/validar_token.php
+++ PHP/validar_token.php
@@ -9,8 +9,9 @@
         $token = $_POST['token'];
     }
     if(isset($_SESSION['email'])){
-        $email = 'kaparecido483@gmail.com';
+        $email = $_SESSION['email'];
     }
+
     if($email != "" && $token != ""){
         $sql = "SELECT * FROM token WHERE email ='" . $email . "' AND token ='" . $token . "'";
         $resultado = $conn->query($sql);
@@ -19,75 +20,92 @@
                 $data_criacao = $linha['data_criacao'];
                 $valido = $linha['valido'];
             }
-            $validacao = true;
-            //Separando em variaveis a data que vem do token no banco
-            //echo $data_criacao . "<br>";
-            //echo $valido . "<br>";
-            list($ano, $mes, $dia) = explode("-", $data_criacao);
-            list($dia, $tempo_criacao) = explode(" ", $dia);
-            list($horas, $minutos, $segundos) = explode(":", $tempo_criacao);
-            //echo "$ano $mes $dia - $horas $minutos $segundos" . "<br>";
+            if($valido){
+                $validacao = true;
+                //Separando em variaveis a data que vem do token no banco
+                //echo $data_criacao . "<br>";
+                //echo $valido . "<br>";
+                list($ano, $mes, $dia) = explode("-", $data_criacao);
+                list($dia, $tempo_criacao) = explode(" ", $dia);
+                list($horas, $minutos, $segundos) = explode(":", $tempo_criacao);
+                //echo "$ano $mes $dia - $horas $minutos $segundos" . "<br>";
 
-            //Separando em variaveis a data atual
-            $hoje = date("Y m d H i s");
-            //echo $hoje . "<br>";
-            list($ano_atual, $mes_atual, $dia_atual, $horas_atual, $minutos_atual, $segundos_atual) = explode(" ", $hoje);
+                //Separando em variaveis a data atual
+                $hoje = date("Y m d H i s");
+                //echo $hoje . "<br>";
+                list($ano_atual, $mes_atual, $dia_atual, $horas_atual, $minutos_atual, $segundos_atual) = explode(" ", $hoje);
 
-            echo $ano . "-" . $ano_atual . "<br>";
-            echo $mes . "-" . $mes_atual . "<br>";
-            echo $dia . "-" . $dia_atual . "<br>";
-            echo $horas . "-" . $horas_atual . "<br>";
-            echo $minutos . "-" . $minutos_atual . "<br>";
-
-            if($ano == $ano_atual){
-                if($mes == $mes_atual){
-                    if($dia != $dia_atual){
+                /*
+                echo $ano . "-" . $ano_atual . "<br>";
+                echo $mes . "-" . $mes_atual . "<br>";
+                echo $dia . "-" . $dia_atual . "<br>";
+                echo $horas . "-" . $horas_atual . "<br>";
+                echo $minutos . "-" . $minutos_atual . "<br>";
+                */
+                
+                if($ano == $ano_atual){
+                    if($mes == $mes_atual){
+                        if($dia != $dia_atual){
+                            $validacao = false;
+                            //echo "3". "<br>";
+                        }
+                    }
+                    else{
                         $validacao = false;
-                        echo "3". "<br>";
+                        //echo "2". "<br>";
                     }
                 }
                 else{
                     $validacao = false;
-                    echo "2". "<br>";
-                }
-            }
-            else{
-                $validacao = false;
-                echo "1". "<br>";
-            }
-
-            if($validacao){
-                $min = $minutos + 30;
-                if($min >= 60){
-                    $horas++;
-                    $min =- 60;
+                    //echo "1". "<br>";
                 }
 
-                if($horas == $horas_atual){
-                    echo $min. "<br>";
-                    if($min <= $minutos_atual){
+                if($validacao){
+                    $min = $minutos + 30;
+                    if($min >= 60){
+                        $horas++;
+                        $min =- 60;
+                    }
+
+                    if($horas == $horas_atual){
+                        echo $min. "<br>";
+                        if($min <= $minutos_atual){
+                            $validacao = false;
+                            //echo "5". "<br>";
+                        }
+                    }
+                    else{
                         $validacao = false;
-                        echo "5". "<br>";
+                        //echo "4". "<br>";
                     }
                 }
+
+                if($validacao){
+                    $_SESSION['token'] = $token;
+                    $_SESSION['erro'] = "";
+                }
                 else{
-                    $validacao = false;
-                    echo "4". "<br>";
+                    $erro = "O token foi expirou, tente novamente";
+                    $_SESSION['erro'] = $erro;
+                    $_SESSION['email'] = "";
                 }
             }
-
-            if($validacao){
-                echo "True";
-                $_SESSION['token'] = $token;
-                header('Location: ../HTML/recuperar_senha.php');
-            }
             else{
-                echo "False";
+                $erro = "O token não é mais valido, tente novamente";
+                $_SESSION['erro'] = $erro;
+                $_SESSION['email'] = "";
             }
-
+        }
+        else{
+            $erro = "Email e/ou token não cadastrados no sistema, tente novamente";
+            $_SESSION['erro'] = $erro;
+            $_SESSION['email'] = "";
         }
     }
     else{
-        echo "Envie um email e/ou token para prosseguir";
+        $erro = "Envie um email e/ou token para prosseguir";
+        $_SESSION['erro'] = $erro;
+        $_SESSION['email'] = "";
     }
+    header('Location: ../HTML/recuperar_senha.php');
 ?>
