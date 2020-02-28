f2654a481ab72f0b2cf3086dd756358f78ab7a1a
Kevyn02 <kaparecido483@gmail.com>
2020-02-28 13:55:54 -0300

    Resolvendo o erro no layout do contato

--- HTML/contato.php
+++ HTML/contato.php
@@ -53,7 +53,7 @@
                     include("./parts/erro.php");
                 }
             ?>
-            <div class="col-6">
+            <div class="col-sm-auto col-md-6">
                 <form class="form-horizontal" action="PHP/enviar_email.php" method="post">
                     <div class="text-center">
                         <h1>Contato</h1>
@@ -61,13 +61,15 @@
                     <div class="form-group">
                         <label for="nome">Nome</label>
                         <div class="col-auto">
-                            <input class="form-control" type="nome" name="nome" id="nome" value="<?php echo $nome;?>" required>
+                            <input class="form-control" type="nome" name="nome" id="nome" value="<?php echo $nome;?>"
+                                required>
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="email">Email</label>
                         <div class="col-auto">
-                            <input class="form-control" type="email" name="email" id="email" value="<?php echo $email;?>" required>
+                            <input class="form-control" type="email" name="email" id="email"
+                                value="<?php echo $email;?>" required>
                         </div>
                     </div>
                     <div class="form-group">
@@ -90,8 +92,11 @@
                     </div>
                 </form>
             </div>
-            <div class="col-6">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3698.6484369460777!2d-47.89359248501157!3d-22.02477761252949!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b87722afe006bb%3A0x4a8b254e7543696!2sSenac%20S%C3%A3o%20Carlos!5e0!3m2!1spt-BR!2sbr!4v1581451958431!5m2!1spt-BR!2sbr" width="600" height="650" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+            <div class="col-sm-12 col-md-6">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3698.6484369460777!2d-47.89359248501157!3d-22.02477761252949!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b87722afe006bb%3A0x4a8b254e7543696!2sSenac%20S%C3%A3o%20Carlos!5e0!3m2!1spt-BR!2sbr!4v1581451958431!5m2!1spt-BR!2sbr"
+                    width="100%" height="650" frameborder="0" style="border:0;"
+                    allowfullscreen=""></iframe>
             </div>
         </div>
     </main>
